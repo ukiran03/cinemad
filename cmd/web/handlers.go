c516268b8b2d2726071490180ef33b4c69df4ebf
ukiran03 <ushakiranreddi@gmail.com>
2026-09-24 07:59:44 +0530

    feat: add payment succeed, generates receipt handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -24,3 +24,49 @@ func (app *application) VirtualTerminal(
 
 	app.logger.Info("Hit the handler")
 }
+
+func (app *application) PaymentSucceeded(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	err := r.ParseForm()
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(
+			w,
+			fmt.Sprintf("Error: %v\n", err),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pm"] = paymentMethod
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+
+	// render templates
+	tData := app.NewTemplateData(r)
+	tData.Data = data
+
+	err = pages.PaymentSucceededPage(tData).Render(r.Context(), w)
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("Error: %v\n", err),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,5 +10,6 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/vt", app.VirtualTerminal)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	return mux
 }
