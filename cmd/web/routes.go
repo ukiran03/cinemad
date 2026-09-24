63c16e38e2e99b2ea8107bf188189ed2b8e53758
ukiran03 <ushakiranreddi@gmail.com>
2026-09-24 11:01:38 +0530

    feat(web): serve static files from embed.FS

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"io/fs"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"ukiran03.com/cinemad/cmd/web/ui"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
+	// A sub-file system for the embedded "static" directory
+	staticFS, err := fs.Sub(ui.Files, "static")
+	if err != nil {
+		panic(err)
+	}
+
+	// Serve the embedded static files
+	mux.Handle(
+		"/static/*",
+		http.StripPrefix("/static", http.FileServer(http.FS(staticFS))),
+	)
+
+	// Application routes
 	mux.Get("/vt", app.VirtualTerminal)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+
 	return mux
 }
